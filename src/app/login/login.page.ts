a2dbd06acc7eaec6c20bf9afeb410c17ad46feeb
Francis Andree Fiallos Silva <fafs.1405@gmail.com>
2025-01-29 23:11:17 -0500

    2

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
+import { LoginService } from '../services/login/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  standalone: false
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  cedula: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
-  ngOnInit() {
+  async login() {
+    if (!this.cedula || !this.password) {
+      this.showAlert('Error', 'Ingrese su cédula y contraseña.');
+      return;
+    }
+
+    this.loginService.login(this.cedula, this.password).subscribe(async response => {
+      if (response.respuesta.estado) {
+        localStorage.setItem('userData', JSON.stringify(response.data));
+        this.router.navigate(['/home']); // Redirige al Home
+      } else {
+        this.showAlert('Error', 'Credenciales incorrectas.');
+      }
+    }, error => {
+      this.showAlert('Error', 'No se pudo conectar con el servidor.');
+    });
   }
 
+  async showAlert(title: string, message: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
---
+++ src/app/services/login/login.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private API_URL = 'https://tuservidor.com/api.php'; // Cambia esto a la URL de tu backend
+
+  constructor(private http: HttpClient) {}
+
+  // Método para iniciar sesión con Headers personalizados
+  login(cedula: string, password: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE'
+    });
+
+    return this.http.post(this.API_URL, {
+      accion: 'login',
+      cedula,
+      password
+    }, { headers });
+  }
+}
